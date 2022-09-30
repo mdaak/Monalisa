85948329c7697a441f00cbeb8b9a2e7b36dbc794
mdaak <md.aak3@gamil.com>
2022-09-30 21:26:51 +0600

    about page complete

--- src/componants/componant/DualDiv.js
+++ src/componants/componant/DualDiv.js
@@ -13,10 +13,10 @@ if(id%2===0){
 }
   return (
     <div style={changeDirection(id)} className={styles.dual_div}>
-        <div className={styles.dual_image} style={{border:'2px solid red'}}>
+        <div className={styles.dual_image}>
             <img src={image} alt='hand shake'/>
         </div>
-        <div className={styles.dual_text} style={{border:'2px solid red'}}>
+        <div className={styles.dual_text}>
             <div>
             <p className={styles.title}>{title}</p>
             <p className={styles.heading}>{heading}</p>
