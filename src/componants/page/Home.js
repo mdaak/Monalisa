9968a3767f83aa892477d5b81fc66ef243eb87ee
mdaak <md.aak3@gamil.com>
2022-10-02 00:35:59 +0600

    building project

--- src/componants/page/Home.js
+++ src/componants/page/Home.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import CardSection from '../componant/cardFile/CardSection';
 import CarouselComp from '../componant/carouselFile/CarouselComp';
-// import Layout from '../Layout';
 
 
 function Home() {
   return (
     <>
-      {/* <Layout> */}
       <CarouselComp/>
       <CardSection/>
-    {/* </Layout> */}
     </>
   
   )
